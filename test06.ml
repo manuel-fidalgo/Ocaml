040b3f59877c77bfbb74926afef7925796947264
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-11-23 10:24:40 +0100

    Mas trabajo agnadido enunciado del dia 23/11

--- test06.ml
+++ test06.ml
@@ -3,9 +3,23 @@ let cuadrado x = x*x
 
 let rec fun_0 x =
 	if x = 0 then 1
-	else fun_0(x*(x-1));;
+	else x*fun_0(x-1);;
 
-let rec fun_1 x = function
-0 -> 1
-| fun_1(x*(n-1));;
+
+let rec fun_1 = function
+	0 -> 1
+	| x -> x*(fun_1(x-1));;
+
+
+for i=1 to 18 do
+	(*
+	print_int (fun_0 i);
+	print_string " - ";
+	print_int (fun_1 i);
+	print_newline(); 
+*)
+	Printf.printf("numero-> %d %d - %d\n")i(fun_0 i)(fun_1 i);
+	 
+done;
+
 
---
+++ test07.ml
@@ -0,0 +1,12 @@
+(*Eserzisio fatto a clase il giorno 23/11*)
+(*
+	m=0 una sola []
+	la lista que contiene todas las listas de longituz 0 [[]];;
+	m>0{
+		esampio m = 3; [[x,x][x,y][y,x][y,y]]
+		[[x,x,x][x,x,y][x,y,x][x,y,y][y,x,x][y,x,y][y,y,x][y,y,y]] //Metes x o y sobre las calculadas anteriormente (Recursividad)
+		let result to(n-1)xy in
+		List.map(cons x)result 
+		let cons x y = x y
+	}
+*)
