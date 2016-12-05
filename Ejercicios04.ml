7efa117ec9cb1fc55568983ee2cc57fbb4128edf
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-12-05 15:51:38 +0100

    finished exercise number 4

--- Ejercicios04.ml
+++ Ejercicios04.ml
@@ -15,9 +15,9 @@ else
 
 (*---Probamos que todo sea correcto---*)
 
-let de_7 = [7;7;7;7;7;7;7];;
-let de_14 = [1;2;3;4;5;6;7;1;2;3;4;5;6;7];;
-let de_21 = [1;2;3;4;5;6;7;1;2;3;4;5;6;7;1;2;3;4;5;6;7];;
+let de_7 = [7;7;7;7;7;7;9];;
+let de_14 = [1;2;3;4;0;6;7;1;2;3;4;5;6;7];;
+let de_21 = [1;2;3;4;5;6;7;456;2;3;4;5;6;7;1;2;3;4;5;6;458];;
 
 Printf.printf "De 7 %d\n" (length de_7);;
 Printf.printf "De 14 %d\n" (length de_14);;
@@ -47,22 +47,54 @@ Printf.printf "\n--------------------------\n"
 let rec max_lista lst num =
 	match lst with
 	| [] -> num
-	| _ -> choose (lst num)
+	| _ -> choose lst num
 and choose lst num =
 	if get_fist lst > num then
-		max_lista((cut lst) (get_fist lst))
+		max_lista (cut lst) (get_fist lst)
 	else
-		max_lista((cut lst) num)
+		max_lista (cut lst) num
 ;;
 
-(**Funzione per vedere si la lista è vuota*)
+(** Funzione per vedere si la lista è vuota *)
 let rec max_list lst =
 	match lst with
 	| [] -> failwith "Lista vuota"
 	| _ -> max_lista lst 0
 ;;
 
-Printf.printf "De 7 %d\n" (max_lista de_7);;
-Printf.printf "De 14 %d\n" (max_lista de_14);;
-Printf.printf "De 21 %d\n" (max_lista de_21);;
+Printf.printf "De 7 %d\n" (max_list de_7);;
+Printf.printf "De 14 %d\n" (max_list de_14);;
+Printf.printf "De 21 %d\n" (max_list de_21);;
+(* Printf.printf "De vacia %d\n" (max_list []);; va bene *)
+Printf.printf "\n--------------------------\n"
+
+(*drop: int -> ’a list -> ’a list, tale che drop n lst = lista
+che si ottiene da lst togliendone i primi n elementi. Se il numero
+di elementi di lst è minore di n (oppure uguale a n), allora drop n
+lst = [].*)(*cout = numero di elementi tagliati*)
+
+let add x = x+1
+;;
+
+let rec print_list = function
+ [] -> Printf.printf "-\n"
+| head::tail -> Printf.printf "%d," head ; print_list tail
+;; 
+
+let rec drop_rec n cout lst =
+	match cout >= n with
+	| true -> lst
+	| _ -> drop_rec n (add cout) (cut lst)
+;;
+
+let drop n lst =
+	if n >= (length lst) then
+		[]
+	else
+		drop_rec n 0 lst
+;;
+
+print_list (drop 3 de_7);;
+print_list (drop 5 de_14);;
+print_list (drop 90 de_21);;
 Printf.printf "\n--------------------------\n"
