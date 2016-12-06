e5891826a271a4d59e3258558dc0e8c76bbb10fc
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-12-06 13:24:44 +0100

    To the exercise number 7

--- Ejercicios04.ml
+++ Ejercicios04.ml
@@ -1,3 +1,8 @@
+(*
+let clear x = Sys.command "clear"+x
+;;
+clear 0;;
+*)
 (*Esercizi porposti 4*)
 (* length 'a lsit -> int *)
 let cut lst = 
@@ -19,6 +24,7 @@ let de_7 = [7;7;7;7;7;7;9];;
 let de_14 = [1;2;3;4;0;6;7;1;2;3;4;5;6;7];;
 let de_21 = [1;2;3;4;5;6;7;456;2;3;4;5;6;7;1;2;3;4;5;6;458];;
 
+Printf.printf "\n-------Ejercicio 1--------\n";;
 Printf.printf "De 7 %d\n" (length de_7);;
 Printf.printf "De 14 %d\n" (length de_14);;
 Printf.printf "De 21 %d\n" (length de_21);;
@@ -37,6 +43,7 @@ let rec summ_of lst =
 	| _ -> (get_fist lst) + (summ_of (cut lst))
 ;;
 
+Printf.printf "\n-------Ejercicio 2--------\n";;
 Printf.printf "De 7 %d\n" (summ_of de_7);;
 Printf.printf "De 14 %d\n" (summ_of de_14);;
 Printf.printf "De 21 %d\n" (summ_of de_21);;
@@ -61,7 +68,7 @@ let rec max_list lst =
 	| [] -> failwith "Lista vuota"
 	| _ -> max_lista lst 0
 ;;
-
+Printf.printf "\n-------Ejercicio 3--------\n";;
 Printf.printf "De 7 %d\n" (max_list de_7);;
 Printf.printf "De 14 %d\n" (max_list de_14);;
 Printf.printf "De 21 %d\n" (max_list de_21);;
@@ -76,10 +83,14 @@ lst = [].*)(*cout = numero di elementi tagliati*)
 let add x = x+1
 ;;
 
-let rec print_list = function
-[] -> Printf.printf "-\n"
-| head::tail -> Printf.printf "%d," head ; print_list tail
+let rec print_list_rec = function
+[] -> Printf.printf "]\n"
+| head::tail -> Printf.printf "%d," head ; print_list_rec tail
 ;; 
+
+let print_list lst = 
+	Printf.printf "["; print_list_rec lst
+;;
 
 let rec drop_rec n cout lst =
 	match cout >= n with
@@ -93,11 +104,11 @@ let drop n lst =
 else
 	drop_rec n 0 lst
 ;;
-
+Printf.printf "\n-------Ejercicio 4--------\n";;
 print_list (drop 3 de_7);;
 print_list (drop 5 de_14);;
 print_list (drop 90 de_21);;
-Printf.printf "\n--------------------------\n"
+Printf.printf "\n--------------------------\n";;
 
 (*
 append: ’a list -> ’a list -> ’a list. Se @ non fosse prede-
@@ -105,14 +116,57 @@ finito, come si potrebbe definire (utilizzando solo i costruttori delle
 liste)?
 *)
 
+let reverse lst = 
+	List.rev lst
+;;
+
 let rec insert_end lst element =
 	match lst with
 	| [] -> [element]
-	| head::tail -> head :: (insert_end tail element)
+	| head::tail -> head::(insert_end tail element)
 ;;
 
-let rec concat lst_1 lst_2 =
+let rec concat_rec lst_1 lst_2 =
 	match lst_2 with
-	| [] -> []
-	| x::tail -> insert_end(x tail)
-;; 
+	| [] -> lst_1
+	| head::tail -> concat_rec (insert_end lst_1 head) tail
+;;
+
+let concat lst_1 lst_2 =
+	concat_rec lst_1 lst_2
+;;
+Printf.printf "\n-------Ejercicio 5--------\n";;
+print_list (concat de_7 de_14);;
+print_list (concat [1;2;3;4;5] [4;3;2;1]);;
+Printf.printf "\n--------------------------\n";;
+
+let duplica_elemento x = 
+	x*2
+;;
+let duplica lst = 
+	List.map duplica_elemento lst
+;;
+Printf.printf "\n-------Ejercicio 6--------\n";;
+print_list (duplica de_7);;
+Printf.printf "\n--------------------------\n";;
+
+(*
+	pair_with x list -> [(x,l0);(x,l1);...;(x,ln)]
+*)
+let rec pair_with_rec n lst_1 lst_2=
+	match lst_1 with
+	| [] -> lst_2
+	| head::tail -> (n,head)::lst_2;
+					pair_with_rec n tail lst_2
+;;
+let pair_with n lst =
+	pair_with_rec n lst []
+;;
+
+Printf.printf "\n-------Ejercicio 7--------\n";;
+(*
+print_list_tuple (pair_with 5 de_7);;
+print_list_tuple (pair_with 0 de_14);;
+print_list_tuple (pair_with 2 de_21);;
+*)
+Printf.printf "\n--------------------------\n";;
---
+++ test10.ml
@@ -0,0 +1,10 @@
+(*
+	propargate:
+	Leaf n -> leaf n
+	Node ((p,n),subtrees)	muovi= aplicacino de propiedad a a t di subtrees
+							mouvi= List.map propagate subtre
+									x = mas y min tre i valori delle nodici degli alb in muovi (if p = max then maxl else mine)(valoridici nuovi)
+							subst = [ t1,...,tn] 
+							[propagate t1,...,propagate tn]
+
+*)
