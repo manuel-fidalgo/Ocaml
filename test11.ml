3344407b401e7a978a313e87979bd230eafe4505
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-12-15 19:23:31 +0100

    Terminada el mapeo mediante recursion de cola

--- test11.ml
+++ test11.ml
@@ -5,25 +5,29 @@
 	’a, and it returns a list containing elements of type ’b.
 
 *)
-(*f function que se aplicara sobre todos los elementos de la lista*)
+(*f -> function que se aplicara sobre todos los elementos de la lista*)
+
+(*
 let map f = function
  [] -> []
-| head::tail -> f head :: map f tail;;
+| head::tail -> f head :: map f tail
+;;
+*)
 
-(*tail recursive*)
+(*------------------
+  - TAIL RECURSIVE -
+  ------------------*)
 
 (*Invierte el orden de una lista*)
 let rec rev accum = function
 h :: t -> rev (h :: accum) t
 | [] -> accum
 ;;
-(*Other way -> list_1 lista a reversar, lista_2 acumulador*)
-let rec reverse list_1 list_2 = 
-	match list_1 with
-	| [] -> list_2
-	| head::tail -> reverse tail (head::list_2)
-;;
-(*Comentario de como funciona*)
+
+(*f funcion a aplicar a cada uno de los elementos,
+accum ira contengiendo cada 
+uno de los elementos f(x) de manera inversa
+ya que se añaden por el principio*)
 let rec rev_map f accum = function
 h :: t -> rev_map f (f h :: accum) t
 | [] -> accum
@@ -31,19 +35,28 @@ h :: t -> rev_map f (f h :: accum) t
 
 (*f fucion y l la lista*)
 let map f l = rev [] (rev_map f [] l)
+;;
+(*-----------------------------
+  - SOLUCION PROPUESTA POR MI -
+  -----------------------------*)
 
-
-(*list_1 lista a reversar, lista_2 acumulador*)
-let rec reverse list_1 list_2 = 
+(*Otro modo -> list_1 lista a invertir, lista_2 acumulador*)
+let rec reverse_ list_1 list_2 = 
 	match list_1 with
 	| [] -> list_2
-	| head::tail -> reverse tail (head::list_2)
+	| head::tail -> reverse_ tail (head::list_2)
 ;;
-(* f funcion a aplicar sobre cada elemento lst_1 lista para ser mapeada, lst_2 lista acumuladora en forma recursiva*)
-let rec rev_map f list_1 list_2 = 
+(* f -> funcion a aplicar sobre cada elemento,
+lst_1-> lista para ser mapeada,
+lst_2 -> lista acumuladora en forma recursiva*)
+let rec rev_map_ f list_1 list_2 = 
 	match list_1 with
-	| patt -> expr
-	| _ -> expr2
+	| [] -> list_2
+	| head::tail -> rev_map_ f tail ((f head)::list_2)
+;;
+
+let map_ f lst = reverse_ (rev_map_ f lst []) []
+;;
 
 
 
