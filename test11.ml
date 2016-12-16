85ce82cb174a4a59b49d1f2990ee71b967d5b5f8
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2016-12-16 15:41:17 +0100

    Unions and more tail recursion

--- test11.ml
+++ test11.ml
@@ -58,12 +58,53 @@ let rec rev_map_ f list_1 list_2 =
 let map_ f lst = reverse_ (rev_map_ f lst []) []
 ;;
 
+(*------------------
+  - Bases de datos -
+  ------------------*)
+let db = [
+"John", "x3456", 50.1;
+"Jane", "x1234", 107.3;
+"Joan", "unlisted", 12.7
+]
+
+let get_name (x,_,_) = x
+;;
+let get_id (_,x,_) = x
+;;
+let get_salary (_,_,x) = x
+;;
+let compare_Strings s1 s2 = true
+;;
 
 
+(*Toma la base de datos y el nombre y develve el salario del primer match*)
+(*String -> float*)
+let rec find_salary name database = 
+	match database with
+	| [] -> failwith "Not found"
+	| head::tail -> if ((compare name (get_name head))=0) 
+						then get_salary head
+					else 
+						find_salary name tail
+;;
 
+Printf.printf "Salario de John %.3f\n" (find_salary "John" db);;
+Printf.printf "Salario de Jane %.3f\n" (find_salary "Jane" db);;
+Printf.printf "Salario de Joan %.3f\n" (find_salary "Joan" db);;
 
+(*----------
+  - Append -
+  ----------*)
 
+let l1 = 1::2::3::4::5::[];;
+let l2 = 6::7::8::9::10::[];;
 
+let rec append lst_1 lst_2 =
+	match lst_1 with
+	| head::tail -> head :: append tail lst_2
+	| _ -> lst_2
+;;
+append l1 l2;;
 
 
 
