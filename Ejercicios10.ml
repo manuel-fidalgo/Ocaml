c71d3d263b6688349121c998817077a17c550cd9
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2017-01-30 16:48:09 +0100

    Finished and tested exercises no 1 and no 2

--- Ejercicios10.ml
+++ Ejercicios10.ml
@@ -22,34 +22,55 @@ let vicini nodo grafo =
 (*Cojo el nodo1, calculo todos los hijos. para cada nodo hijo miro si es nodo2
 si no lo es miro a ver si ya esta visitado, si no lo esta buscamos sobre ese hijo*)
 
+let graph = (1,2)::(2,4)::(1,4)::(4,3)::(3,5)::[];;
+let c = (1,2)::(2,3)::(3,4)::(4,1)::[];;
+let l = [(1,4);(2,4);(3,4);(4,5)];;
+let m = [(1,4);(2,4);(3,4);(4,5);(5,6);(6,7);(7,1)];;
+
+
 let rec test_connessi_rec grafo actual fin visitados =
 																  (*añadimos el nodo padre a visitados*)
-		iter_list_children (vicini actual grafo) grafo actual fin 
-							(visitados@actual) (*iteramos sobre todos los hijos del nodo visitado*)
+		iter_list_children (sucesores actual grafo) grafo actual fin 
+							(actual::visitados) (*iteramos sobre todos los hijos del nodo visitado*)
 
 and iter_list_children children grafo actual fin visitados =
 	match children with
 	| [] -> false
 	| head::tail -> if head = fin then (*Nodo final encontrado*)
-						
 						true
-
-					else( if (not (List.mem head visitados)) then (*caso en que el nodo sobre el que estamos iterando no este todavia en visitado*)
-								
-								(test_connessi_rec grafo head fin visitado) ||
-								(iter_list_children tail grafo actual fin visitados)
-
-							else
-								
-								(iter_list_children tail grafo actual fin visitados)
-			
-					)
+					else if (List.mem head visitados) then (*El nodo ya esta visitado, procedemos a iterar sobre los demas*)
+							(iter_list_children tail grafo actual fin visitados)
+						else
+					 		(test_connessi_rec grafo head fin visitados) || (*Nodo no visitado, calculamos sus hijos y luego *)
+							(iter_list_children tail grafo actual fin visitados)
 
 let test_connessi grafo init fin = (*Inicializamos sin ningun nodo visitado*)
 	test_connessi_rec grafo init fin [];;
 
+(* Clave API google maps -> AIzaSyABEtbg05ASIGVEcBq0EHVtVU5K05S8Uuw *)
 
+let fstnode grafo =
+	match grafo with
+	|(x,y)::tail -> x
+	| [] -> failwith "No such node" 
+;;
 
+let rec esiste_ciclo_rec grafo actual visitados =
+																  
+		iter_list_children (sucesores actual grafo) grafo actual  
+		(*añadimos el nodo padre a visitados*)(actual::visitados) (*iteramos sobre todos los hijos del nodo visitado*)
+
+and iter_list_children children grafo actual  visitados =
+	match children with
+	| [] -> false
+	| head::tail -> 
+					if (List.mem head visitados) then (*El nodo ya esta visitado, se considera que almenos tiene un arco*)
+						true
+					else 		(*Calculamos sobre el no visitado y Iteramos sobre los demas nodos *)
+					 	(esiste_ciclo_rec grafo head  visitados) || (iter_list_children tail grafo actual visitados)
+
+let esiste_ciclo grafo = (*Inicializamos sin ningun nodo visitado*)
+	esiste_ciclo_rec grafo (fstnode grafo) [];;
 
 
 
